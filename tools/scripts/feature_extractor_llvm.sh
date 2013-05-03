e3f5676d2d8b19b56057c0a756dba3f4d814f8b9
Hari Seldon <alberto.magni86@gmail.com>
2013-05-03 16:42:55 +0100

    Adding scripts

---
+++ tools/scripts/feature_extractor_llvm.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+CLANG=clang
+OPT=opt
+LLVM_DIS=llvm-dis
+AXTOR=axtor
+
+INPUT_FILE=$1
+KERNEL_NAME=$2
+COARSENING_DIRECTION=$3
+COARSENING_FACTOR=$4
+COARSENING_STRIDE=$5
+
+OCLDEF=/home/s1158370/src/myclash/axtor_scripts/ocldef.h
+OPTIMIZATION=-O0
+TMP_FILE=/tmp/tc_tmp${RANDOM}.cl
+OUTPUT_FILE=/tmp/tc_output${RANDOM}.cl
+
+$CLANG -x cl \
+       -target nvptx \
+       -include ${OCLDEF} \
+       ${OPTIMIZATION} \
+       ${INPUT_FILE} \
+       -S -emit-llvm -fno-builtin -o - | \
+$OPT -mem2reg \
+     -stats \
+     -inline -inline-threshold=10000 \
+     -instnamer -load ~/root/lib/LLVMTC.so -be -tc \
+     -coarsening-factor ${COARSENING_FACTOR} \
+     -coarsening-direction ${COARSENING_DIRECTION} \
+     -coarsening-stride ${COARSENING_STRIDE} -o - | \
+${LLVM_DIS} -o ${TMP_FILE}
+${AXTOR} ${TMP_FILE} -m OCL -o ${OUTPUT_FILE} 
+
+rm ${TMP_FILE}
+
+$CLANG -x cl \
+       -target nvptx \
+       -include ${OCLDEF} \
+       ${OPTIMIZATION} \
+       ${OUTPUT_FILE} \
+       -S -emit-llvm -fno-builtin -o - | \
+$OPT -mem2reg \
+     -stats \
+     -load ~/root/lib/LLVMTC.so -opencl_instcount -count-kernel-name ${KERNEL_NAME} \
+     -o - > /dev/null
+
+rm ${OUTPUT_FILE}
---
+++ tools/scripts/feature_extractor_std.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+CLANG=clang
+OPT=opt
+LLVM_DIS=llvm-dis
+
+INPUT_FILE=$1
+KERNEL_NAME=$2
+COARSENING_DIRECTION=$3
+
+OCLDEF=/home/s1158370/src/myclash/axtor_scripts/ocldef.h
+OPTIMIZATION=-O3
+
+$CLANG -x cl \
+       -target nvptx \
+       -include ${OCLDEF} \
+       -O0 \
+       ${INPUT_FILE} \
+       -S -emit-llvm -fno-builtin -o - |  
+$OPT -instnamer \
+     -mem2reg \
+     -inline -inline-threshold=10000 \
+     -O3 -o - |
+$LLVM_DIS -o -
+#     -load /home/s1158370/root/lib/feature_extraction.so -opencl-instcount -count-kernel-name ${KERNEL_NAME} 2> /dev/null
