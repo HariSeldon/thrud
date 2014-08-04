e35a4a922736824ad84e3e4e159207f4158bf410
Alberto Magni <alberto.magni86@gmail.com>
2014-08-04 12:17:31 +0100

    Adding test directory

---
+++ tests/vectorization/run_all.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+
+CLANG=clang
+OPT=opt
+LLVM_DIS=llvm-dis
+AXTOR=axtor
+LIB_THRUD=$HOME/root/lib/libThrud.so
+TMP_FILE=/tmp/tc_tmp${RANDOM}.cl
+
+OCLDEF=$HOME/src/thrud/tools/scripts/opencl_spir.h
+OPTIMIZATION=-O0
+
+function runTest {
+  INPUT_FILE=$1
+  KERNEL_NAME=$2
+  VECTOR_WIDTH=$3
+  VECTOR_DIRECTION=$4
+
+  RED='\e[0;31m'
+  GREEN='\e[0;32m'
+  BLANK='\e[0m'
+
+  $CLANG -x cl \
+         -target spir \
+         -include ${OCLDEF} \
+         ${OPTIMIZATION} \
+         ${INPUT_FILE} \
+         -S -emit-llvm -fno-builtin -o - | \
+  $OPT -mem2reg -instnamer \
+       -load $LIB_THRUD -be -tv \
+       -vectorizing-width ${VECTOR_WIDTH} \
+       -vectorizing-direction ${VECTOR_DIRECTION} \
+       -div-region-mgt classic \
+       -kernel-name ${KERNEL_NAME} \
+       -o /dev/null 2> /dev/null
+  if [ $? == 0 ] 
+  then 
+    echo -e "${GREEN}runTest $INPUT_FILE $KERNEL_NAME $VECTOR_WIDTH $VECTOR_DIRECTION Ok!${BLANK}"
+  else
+    echo -e "${RED}runTest $INPUT_FILE $KERNEL_NAME $VECTOR_WIDTH $VECTOR_DIRECTION Error${BLANK}"
+  fi 
+}
+
+runTest kernels/memset.cl memset1 4 0 
+runTest kernels/memset.cl memset2 4 0 
+runTest kernels/memset.cl memset2 4 1
+runTest kernels/bruno_examples.cl vectorTest 4 0
+runTest kernels/bruno_examples.cl vectorTest 4 1
+runTest kernels/bruno_examples.cl vectorTest2 4 0
+runTest kernels/bruno_examples.cl vectorTest2 4 1
+runTest kernels/mm.cl mm 4 0
+runTest kernels/mm.cl mm 4 1
+runTest kernels/mt.cl mt 4 0
+runTest kernels/mt.cl mt 4 1
